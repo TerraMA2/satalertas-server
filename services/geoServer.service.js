2a08dc641206d50b5c318af5ada3089801ad4275
miguel <miguelthiago.ufla@gmail.com>
2019-12-16 22:16:20 -0300

    Created all view of filter AQ

--- services/geoServer.service.js
+++ services/geoServer.service.js
@@ -93,7 +93,7 @@ module.exports = geoServerService = {
 
       const json = await axios.get( url, CONFIG_JSON).then(resp => resp ).catch(err => err);
 
-      const method = (json.data && json && json.status && json.status === 200) ? 'put' : 'post';
+      const method = (json.data && json && json.status && json.status === 200 && json.data) ? 'put' : 'post';
 
       const jsonView = geoServerUtil.setJsonView(json, view);
       response.push(await this.saveGeoServer(view.name, method, urli, jsonView, CONFIG_JSON) );
