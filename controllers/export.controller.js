bce65840bae99f231c65bf6fa532b65c1498dfdd
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-08-14 15:47:54 -0300

    Refactored export function

--- controllers/export.controller.js
+++ controllers/export.controller.js
@@ -1,9 +1,10 @@
 const Result = require(__dirname + `/../utils/result`);
 const ExportService = require("../services/export.service.js");
+const fs = require("fs");
 
 exports.get = async (req, res) => {
     try {
-        const { filePath } = await ExportService.get(req.body.params);
+        const { filePath } = await ExportService.get(req.body.params.params);
         const fileBase64 = fs.readFileSync(filePath, 'base64')
         res.json(Result.ok(fileBase64));
     } catch (e) {
