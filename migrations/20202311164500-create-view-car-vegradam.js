07545404f495ed8028ca8fd0f10f8cd9a08f4aac
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-11-25 08:06:44 -0300

    Added car x vegradam view to migration

---
+++ migrations/20202311164500-create-view-car-vegradam.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.query(`
+        CREATE or REPLACE VIEW car_x_vegradam AS
+        SELECT c.gid, c.numero_do1, c.numero_do2, v.fisionomia, c.area_ha_,
+                 SUM(ST_Area(ST_Intersection(c.geom, v.geom)::geography) / 10000.0) AS area_ha_car_vegradam
+        FROM de_car_validado_sema AS c, de_veg_radambr AS v
+        WHERE ST_Intersects(c.geom, v.geom)
+        GROUP BY v.fisionomia, c.area_ha_, c.gid, c.numero_do1, c.numero_do2
+    `);
+  },
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.query(`DROP VIEW car_x_vegradam`);
+  }
+};
