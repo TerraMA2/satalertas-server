b10c952ca756ac6e6fa85f89f79918478ee57c0c
Miguel <miguelthiago.ufla@gmail.com>
2020-12-10 16:48:39 -0300

    Fixed Prodes, adjusted points.

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cors())
 app.use(compression({}))
 app.use(helmet())
 app.use(morgan('combined', {}))
-app.use(express.json({limit: '100mb'}))
+app.use(express.json({limit: '200mb', inflate: true, strict: true, type: 'application/json'}))
 
 app.use(basePath+'/view', viewRouter)
 app.use(basePath+'/geoserver', geoserverRouter)
