7f2b62bc88d705c01eaab71bcf85d80ea17388d5
Kyle Felipe <kylefelipe@gmail.com>
2021-05-17 19:28:52 -0300

    Changes model to use class

--- models/rel-group-view.model.js
+++ models/rel-group-view.model.js
@@ -1,33 +1,51 @@
 'use strict';
+const { Model } = require('sequelize');
+const allModels = require('./index')
+
 module.exports = (sequelize, DataTypes) => {
-  const RelGroupView = sequelize.define('rel_group_view', {
-    id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
+  const { models } = sequelize;
+  class RelGroupView extends Model {
+  }
+
+  RelGroupView.init({
+    id_group: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: models.Group,
+        key: 'id',
+      }
+    },
+    id_view: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: models.view,
+        key: 'id',
+      }
     },
   }, {
+    sequelize,
+    modelName: 'rel_group_view',
+    tableName: 'rel_group_views',
     schema: 'terrama2',
     underscored: true,
     underscoredAll: true,
     timestamps: false
-  })
-  RelGroupView.associate = function(models) {
-    RelGroupView.belongsTo(models.views, {
-      through: 'views',
-      onDelete: 'RESTRICT',
-      foreignKey: 'id_view',
-      as: 'views',
-      otherKey: 'id_view'
-    });
-    RelGroupView.belongsTo(models.groups, {
-      through: 'groups',
-      onDelete: 'RESTRICT',
-      foreignKey: 'id_group',
-      as: 'groups',
-      otherKey: 'id_group'
-    });
-  };
+  });
+  // RelGroupView.associate = function(models) {
+  //   RelGroupView.belongsTo(models.views, {
+  //     through: 'views',
+  //     onDelete: 'NO ACTION',
+  //     foreignKey: 'id_view',
+  //     as: 'views',
+  //     otherKey: 'id_view'
+  //   });
+  //   RelGroupView.belongsTo(models.groups, {
+  //     through: 'groups',
+  //     onDelete: 'CASCADE',
+  //     foreignKey: 'id_group',
+  //     as: 'groups',
+  //     otherKey: 'id_group'
+  //   });
+  // };
   return RelGroupView;
 };
