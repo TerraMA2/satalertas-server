173947fc0a159535638b068a630fd3f789b9d5e1
Kyle Felipe <kylefelipe@gmail.com>
2021-06-23 11:05:34 -0300

    Creates updatesAdvanced

--- services/group-view.service.js
+++ services/group-view.service.js
@@ -1,16 +1,15 @@
 const models = require('../models')
 const RelGroupView = models.rel_group_view
 const View = models.views
-const logger = require('../utils/logger');
+const { msgError } = require('../utils/messageError');
 const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
+const { Op, QueryTypes } = Sequelize;
 
 
 async function getModelFields(model) {
   const fields = await model.describe();
   return fields;
 }
-
 function adjustGroupData(group, relationships = []) {
   const groupData = {
     id: group.id,
@@ -20,83 +19,136 @@ function adjustGroupData(group, relationships = []) {
   if (relationships.length() = 0) {
     return groupData
   }
-  // for 
 
+};
+// use at routes?
+async function getAll() {
+  try {
+    const modelFields = Object.keys(await getModelFields(RelGroupView));
+    const groupViews = await RelGroupView.findAll({
+      attributes: modelFields
+    });
+    for (const groupView of groupViews) {
+      const id = groupView.id_view;
+      groupView.dataValues.view = await View.findByPk(id);
+    }
+    return await groupViews;
+  } catch (e) {
+    // const msgErr = `In unit group-view.service, method getAll:${e}`;
+    // logger.error(msgErr);
+    throw new Error(msgError('group-view.service', 'getAll', e));
+  }
+};
+async function getByIdGroup(idGroup) {
+  try {
+    const modelFields = Object.keys(await getModelFields(RelGroupView))
+    const where = {
+      where: {
+        'id_group': idGroup
+      },
+      order: [['id', 'ASC']]
+    };
 
-}
+    const groupViews = await RelGroupView.findAll({
+      ...where,
+      attributes: modelFields
+    })
+    for (const groupView of groupViews) {
+      const id = groupView.id_view;
+      groupView.dataValues.view = await View.findByPk(id);
+    }
+    return groupViews;
+  } catch (e) {
+    throw new Error(msgError('group-view.service', 'getByIdGroup', e));
+  }
+};
+async function getNotBelongingToTheGroup(idGroup) {
+  try {
+    const idViews = await RelGroupView.findAll({ where: {id_group: idGroup}, attributes: ['id_view'] })
+      .then(list => list.filter(({ id_view}) => id_view).map(({ id_view }) => id_view));
 
-module.exports = GroupService = {
-  async getAll() {
-    try {
-      getModelFields(RelGroupView);
-      console.log("getAll")
-      const groupViews = await RelGroupView.findAll()
-      console.log(groupViews);
-      for (const groupView of groupViews) {
-        const id = groupView.id_view;
-        groupView.dataValues.view = await View.findByPk(id);
+    const option = {
+      where: {
+        id: {[Op.notIn]: idViews }
       }
-      return await groupViews;
-    } catch (e) {
-      const msgErr = `In unit group-view.service, method getAll:${e}`;
-      logger.error(msgErr);
-      throw new Error(msgErr);
-    }
-  },
-  async getByIdGroup(idGroup) {
-    try {
-      const where = {
-        where: {
-          'id_group': idGroup
-        }
-      };
+    };
 
-      const groupViews = await RelGroupView.findAll(where)
-      // const teste = await models.Group.findByPk(idGroup, {include: 'relGroupView'})
-      for (const groupView of groupViews) {
-        const id = groupView.id_view;
-        groupView.dataValues.view = await View.findByPk(id);
-      }
-      return groupViews;
-    } catch (e) {
-      const msgErr = `In unit group-view.service, method getByIdGroup:${e}`;
-      logger.error(msgErr);
-      throw new Error(msgErr);
-    }
-  },
-  async getNotBelongingToTheGroup(idGroup) {
-    try {
-      const idViews = await RelGroupView.findAll({ where: {id_group: idGroup}, attributes: ['id_view'] })
-        .then(list => list.filter(({ id_view}) => id_view).map(({ id_view }) => id_view));
-
-      const option = {
-        where: {
-          id: {[Op.notIn]: idViews }
-        }
-      };
-
-      const listViews = await View.findAll(option);
-      return listViews;
-    } catch (e) {
-      const msgErr = `In unit GroupService.service, method getNotBelongingToTheGroup:${e}`;
-      logger.error(msgErr);
-      throw new Error(msgErr);
-    }
-  },
-  async add(newGroupView) {
+    const listViews = await View.findAll(option);
+    return listViews;
+  } catch (e) {
+    // const msgErr = `In unit GroupService.service, method getNotBelongingToTheGroup:${e}`;
+    // logger.error(msgErr);
+    throw new Error(msgError('group-view.service', 'getNotBelonginToTheGroup', e));
+  }
+};
+async function add(newGroupView) {
+  try {
     const groupView = new RelGroupView({
       idGroup: newGroupView.idGroup,
       idView: newGroupView.idView
     })
     return await RelGroupView.create(groupView.dataValues).then(groupView => groupView.dataValues);
-  },
-  async update(groupViewModify) {
+    
+  } catch (e) {
+    throw new Error(msgError('group-view.service', 'add', e))
+  }
+};
+async function update(groupViewModify) {
+  try {
     const {layers, id_group} = groupViewModify
     const removeRel = await RelGroupView.findAll({where: {id_group: id_group}});
     await removeRel || removeRel.destroy();
     const ddd =  await RelGroupView.bulkCreate(layers);
-  },
-  async delete(id) {
-    await RelGroupView.delete(id);
+  } catch (e) {
+    throw new Error(msgError('group-view.service', 'update', e))
   }
+};
+async function updateAdvanced(groupViewModify) {
+  try {
+    const { editions, id_group } = groupViewModify
+    for (const edition of editions) {
+      let updateSql = `UPDATE ${RelGroupView.getTableName().schema}.${RelGroupView.tableName}\nSET\n`;
+      // Map only sub_layer ID
+      if (edition.sub_layers) {
+        edition.sub_layers = edition.sub_layers.map(item => item.id);
+      }
+      const bind = {id_group};
+      let setSQL = [];
+      Object.keys(edition)
+      .forEach(field => {
+        bind[field] = edition[field];
+        if (field != 'id') {
+          setSQL.push(`\t${field} = $${field}`)
+        }
+      });
+      updateSql += setSQL.join(',\n');
+      updateSql += '\nWHERE id = $id AND id_group = $id_group;'
+      models.sequelize.query(
+        updateSql, {
+          bind,
+          type: QueryTypes.UPDATE,
+        }
+      )        
+    }
+    return await getByIdGroup(id_group);
+  } catch (e) {
+    throw new Error(msgError('group-view.service', 'updateAdvanced', e))
+  }
+};
+async function deleteGroupView(id) {
+  try {
+    await RelGroupView.delete(id);
+  } catch (e) {
+    throw new Error(msgError('group-view.service', 'delete', e))
+  }
+};
+
+module.exports = GroupService = {
+  getAll,
+  getByIdGroup,
+  getNotBelongingToTheGroup,
+  add,
+  update,
+  updateAdvanced,
+  delete: deleteGroupView  
 };
