080b02dff7b4d7ab0b48ca536d7802720b628046
Kyle Felipe <kylefelipe@gmail.com>
2021-07-28 16:51:32 -0300

    Creates migrations

---
+++ migrations/20210728193803-alter-columns-rel-group-views.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return Promise.all([
+      queryInterface.changeColumn('rel_group_views', 'id_view', {
+        onDelete: 'SET NULL',
+      }),
+      queryInterface.changeColumn('rel_group_views', 'id_group', {
+        onDelete: 'CASCADE',
+      }),
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  },
+};
