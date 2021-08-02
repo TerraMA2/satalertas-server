aa0ba2601734f626c7dec49994f45973083d08b0
Kyle Felipe <kylefelipe@gmail.com>
2021-08-02 18:09:40 -0300

    Refactoring RelGroupView.model

--- models/RelGroupView.model.js
+++ models/RelGroupView.model.js
@@ -4,85 +4,87 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   const { models } = sequelize;
   class RelGroupView extends Model {
-    static assosciate(models) {
-    }
+    static assosciate(models) {}
   }
 
-  RelGroupView.init({
-    id_group: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: models.Group,
-        key: 'id',
-      }
-    },
-    id_view: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: models.view,
-        key: 'id',
+  RelGroupView.init(
+    {
+      id_group: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: models.Group,
+          key: 'id',
+        },
+      },
+      id_view: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: models.view,
+          key: 'id',
+        },
       },
       name: {
         type: DataTypes.STRING,
-        allowNull: false,
-        comment: "View name"
+        comment: 'View name',
       },
       short_name: {
         type: DataTypes.STRING,
         allowNull: true,
-        comment: "Short name"
+        comment: 'Short name',
       },
       description: {
         type: DataTypes.TEXT,
-        comment: "View description"
+        comment: 'View description',
       },
       active: {
         type: DataTypes.BOOLEAN,
-        allowNull: false,
         default: true,
-        comment: "It defines view can be used and retrieved. Default is true."
+        comment: 'It defines view can be used and retrieved. Default is true.',
       },
       private: {
         type: DataTypes.BOOLEAN,
-        allowNull: false,
+        // allowNull: false,
         default: false,
-        comment: "It defines if the view is private. Default is false."
+        comment: 'It defines if the view is private. Default is false.',
       },
       schedule_type: {
         type: DataTypes.INTEGER,
-        allowNull: true
+        // allowNull: true,
       },
       source_type: {
         type: DataTypes.INTEGER,
         allowNull: false,
-        comment: "It defines the type of data source that create the view. Alert, Analysis, Static Data or Dynamic Data"
+        comment:
+          'It defines the type of data source that create the view. Alert, Analysis, Static Data or Dynamic Data',
       },
       is_primary: {
         type: DataTypes.BOOLEAN,
-        allowNull: false,
+        // allowNull: false,
         default: false,
-        comment: "It defines if the layer is parent or not. Default is false."
+        comment: 'It defines if the layer is parent or not. Default is false.',
       },
       is_sublayer: {
         type: DataTypes.BOOLEAN,
         default: false,
-        comment: "It defines if the layer is a sublayer or not. Default is false."
+        comment:
+          'It defines if the layer is a sublayer or not. Default is false.',
       },
       sub_layers: {
         type: DataTypes.ARRAY(DataTypes.INTEGER),
-        allowNull: true,
-        comment: "It defines which layers are sub layers of this layer."
-      }
+        // allowNull: true,
+        comment: 'It defines which layers are sub layers of this layer.',
+      },
     },
-  }, {
-    sequelize,
-    modelName: 'RelGroupView',
-    tableName: 'rel_group_views',
-    schema: 'terrama2',
-    underscored: true,
-    underscoredAll: true,
-    timestamps: false
-  });
+    {
+      sequelize,
+      modelName: 'RelGroupView',
+      tableName: 'rel_group_views',
+      schema: 'terrama2',
+      underscored: true,
+      underscoredAll: true,
+      timestamps: false,
+    },
+  );
   // RelGroupView.associate = function(models) {
   //   RelGroupView.belongsTo(models.views, {
   //     through: 'views',
