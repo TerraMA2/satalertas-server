d7f2167e8faf1b8a6b96d95e47ff7cbbd74d3188
Kyle Felipe <kylefelipe@gmail.com>
2021-08-20 13:42:06 -0300

    Refactoring

--- services/group.service.js
+++ services/group.service.js
@@ -1,27 +1,28 @@
-const models = require('../models')
+const models = require('../models');
 const { View, Project, Group, RelGroupView } = models;
 const logger = require('../utils/logger');
+const { msgError } = require('../utils/messageError');
+const { getByGroupId } = require('./group-view.service');
 
 module.exports = GroupService = {
   async getAll() {
     try {
-      return await Group.findAll({raw: true});
+      return await Group.findAll({ raw: true });
     } catch (e) {
       const msgErr = `In unit group.service, method getAll:${e}`;
-      logger.error(msgErr);
-      throw new Error(msgErr);
+      throw new Error(msgError(__dirname, 'getAll', e));
     }
   },
   async getCodGroups() {
     return [
-      {id: 1, cod_group: 'STATIC', label: 'Dado Estático'},
-      {id: 2, cod_group: 'DYNAMIC', label: 'Dado Dinâmico'},
-      {id: 3, cod_group: 'ANALYSIS', label: 'Análise'},
-      {id: 4, cod_group: 'ALERT', label: 'Alerta'},
-      {id: 5, cod_group: 'BURNED', label: 'Análise FOCOS'},
-      {id: 6, cod_group: 'BURNED_AREA', label: 'Análise Área Queimada'},
-      {id: 7, cod_group: 'DETER', label: 'Análise Deter'},
-      {id: 8, cod_group: 'PRODES', label: 'Análise Prodes'},
+      { id: 1, cod_group: 'STATIC', label: 'Dado Estático' },
+      { id: 2, cod_group: 'DYNAMIC', label: 'Dado Dinâmico' },
+      { id: 3, cod_group: 'ANALYSIS', label: 'Análise' },
+      { id: 4, cod_group: 'ALERT', label: 'Alerta' },
+      { id: 5, cod_group: 'BURNED', label: 'Análise FOCOS' },
+      { id: 6, cod_group: 'BURNED_AREA', label: 'Análise Área Queimada' },
+      { id: 7, cod_group: 'DETER', label: 'Análise Deter' },
+      { id: 8, cod_group: 'PRODES', label: 'Análise Prodes' },
     ];
   },
   async getById(id) {
@@ -31,10 +32,9 @@ module.exports = GroupService = {
 
       const group = await Group.findByPk(id).then((result) => result);
       Group.findByPk(id, {
-        include: "relGroupView",
-        raw: true
-      }).then(data => 
-        console.log('Resposta do Group.findByPk: ', data))
+        include: 'relGroupView',
+        raw: true,
+      }).then((data) => console.log('Resposta do Group.findByPk: ', data));
 
       group.dataValues.project = await Project.findByPk(group.idProject);
       where = {
@@ -43,16 +43,15 @@ module.exports = GroupService = {
         },
         // raw: true
       };
-      const relViews = await RelGroupView.findAll(where)
+      const relViews = await RelGroupView.findAll(where);
       // console.log('relViews:', relViews)
-      group.dataValues.relViews = await relViews
+      group.dataValues.relViews = await relViews;
 
-      for(const relViews of group.dataValues.relViews){
+      for (const relViews of group.dataValues.relViews) {
         const id = relViews.idView;
         relViews.dataValues.view = await View.findByPk(id);
       }
       return group;
-
     } catch (e) {
       const msgErr = `In unit group.service, method getById:${e}`;
       logger.error(msgErr);
@@ -65,21 +64,25 @@ module.exports = GroupService = {
       idProject: newGroup.idProject,
       code: newGroup.code,
       dashboard: newGroup.dashboard,
-      active_area: newGroup.activeArea
-    })
-    return await Group.create(group.dataValues).then(group => group.dataValues);
+      active_area: newGroup.activeArea,
+    });
+    return await Group.create(group.dataValues).then(
+      (group) => group.dataValues,
+    );
   },
   async update(groupModify) {
-    const group = await Group.findByPk(groupModify.id);
-    group.name = groupModify.name;
-    group.idProject = groupModify.idProject;
-    group.code = groupModify.code;
-    await group.save();
-    return group.dataValues;
+    try {
+      const { id, ...el } = groupModify;
+      const where = { id }
+      await Group.update(el, { where });
+      return await await Group.findByPk(id, {raw: true});
+    } catch (e) {
+      throw new Error(msgError(__dirname, 'update', e));
+    }
   },
 
   async deleteGroup(groupId) {
     const group = await Group.findByPk(groupId);
-    return await group.destroy().then(result => result);
-  }
+    return await group.destroy().then((result) => result);
+  },
 };
