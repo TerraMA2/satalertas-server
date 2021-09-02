6ec4b029a05ad8611151c160ef3379b03ca151a6
Kyle Felipe <kylefelipe@gmail.com>
2021-09-02 14:03:47 -0300

    Add a new parameter 'colum'

--- utils/result.js
+++ utils/result.js
@@ -1,26 +1,32 @@
 
 
 const result = {
-  ok(json) {
-    return {
+  ok(json, columns = undefined) {
+    const response = {
       status: 200,
       data: json,
       message: "Sucesso!"
     }
+    if (columns) response.columns = columns 
+    return response;
   },
-  created(json) {
-    return {
+  created(json, column = undefined) {
+    const response = {
       status: 201,
       data: json,
       message: "Criado com sucesso!"
     }
+    if (columns) response.columns = columns 
+    return response;
   },
-  err(json) {
-    return {
+  err(json, columns = undefined) {
+    const response = {
       status: 500,
       data: json,
       message: "Erro interno!"
     }
+    if (columns) response.columns = columns 
+    return response;
   }
 };
 module.exports = result;
