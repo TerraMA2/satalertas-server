b14372d01667cf9390ca578746a6fcfc4ecb2518
Kyle Felipe <kylefelipe@gmail.com>
2021-09-02 13:52:49 -0300

    Creates seeders to populate infocolumn tables

--- seeders/20210826190455-get-tables-from-view.js
+++ seeders/20210826190455-get-tables-from-view.js
@@ -1,20 +1,33 @@
 'use strict';
+const oldQuery = `INSERT INTO terrama2.infocolumn_table_list (table_name, type)
+SELECT dsf.value AS "table_name", 'view' AS "type"
+FROM terrama2.data_set_formats AS dsf
+WHERE dsf.key = 'table_name'
+ORDER BY "table_name";`;
+
+const newQueryInsert = `INSERT INTO terrama2.infocolumn_table_list (table_name, type)
+SELECT DISTINCT
+(CASE
+  WHEN vw.source_type = 3 THEN concat(TRIM(dsf.value), '_', analysis.id)
+  ELSE dsf.value
+  END) AS table_name, 'view' AS "type"
+FROM terrama2.views AS vw
+INNER JOIN terrama2.data_sets AS dst ON (vw.data_series_id = dst.data_series_id)
+INNER JOIN terrama2.data_set_formats AS dsf ON (dst.id = dsf.data_set_id)
+LEFT JOIN terrama2.analysis AS analysis ON dsf.data_set_id = analysis.dataset_output
+WHERE dsf.key = 'table_name'
+ORDER BY "table_name";`
+
+const truncateQuery = 'TRUNCATE terrama2.infocolumn_table_list RESTART IDENTITY CASCADE;'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return await queryInterface.sequelize.query(
-      `INSERT INTO terrama2.infocolumn_table_list (table_name, view_id, type)
-  SELECT dsf.value, vw.id, 'view'
-  FROM terrama2.views AS vw
-  INNER JOIN terrama2.data_sets AS dst ON (vw.data_series_id = dst.data_series_id)
-  INNER JOIN terrama2.data_set_formats AS dsf ON (dst.id = dsf.data_set_id)
-  WHERE dsf.key = 'table_name';`
-    )
+    return Promise.all([
+      queryInterface.sequelize.query(newQueryInsert),
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    return await queryInterface.sequelize.query(
-      'TRUNCATE terrama2.infocolumn_table_list RESTART IDENTITY CASCADE;'
-    )
-  }
+    return await queryInterface.sequelize.query(truncateQuery);
+  },
 };
--- seeders/20210826211755-get-colums.js
+++ seeders/20210826211755-get-colums.js
@@ -2,22 +2,25 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return await queryInterface.sequelize.query(
-      `INSERT INTO terrama2.infocolumn_columns_list (table_id, column_name, primary_type, alias)
-      SELECT itl.id,
-        cols.column_name,
-        (CASE WHEN cols.data_type = 'USER-DEFINED'
-          THEN cols.udt_name
-          ELSE cols.data_type
-        END) AS "type_name",
-        cols.column_name
-      FROM terrama2.views AS vw
-      INNER JOIN terrama2.data_sets AS dst ON (vw.data_series_id = dst.data_series_id)
-      INNER JOIN terrama2.data_set_formats AS dsf ON (dst.id = dsf.data_set_id)
-      INNER JOIN information_schema.columns AS cols ON (dsf.value = cols.table_name)
-      INNER JOIN terrama2.infocolumn_table_list AS itl ON (dsf.value = itl.table_name)
-      WHERE dsf.key = 'table_name';`
-    )
+    return Promise.all([
+      queryInterface.sequelize.query(
+        `INSERT INTO terrama2.infocolumn_columns_list (
+          table_id, column_name, primary_type, alias, column_position
+        )
+        SELECT
+        itl.id,
+            cols.column_name,
+            (CASE WHEN cols.data_type = 'USER-DEFINED'
+              THEN cols.udt_name
+              ELSE cols.data_type
+            END) AS "type_name",
+            cols.column_name,
+            cols.ordinal_position
+        FROM terrama2.infocolumn_table_list AS itl
+        INNER JOIN information_schema.columns AS cols ON (itl.table_name = cols.table_name)
+        ORDER BY itl.table_name, cols.ordinal_position, cols.column_name;`
+      )
+    ])
   },
 
   down: async (queryInterface, Sequelize) => {
