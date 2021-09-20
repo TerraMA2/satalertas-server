968a46ae15543f9620e846146230060f51f432a7
Kyle Felipe <kylefelipe@gmail.com>
2021-09-19 23:31:03 -0300

    refactoring filter

--- controllers/group-view.controller.js
+++ controllers/group-view.controller.js
@@ -1,59 +1,62 @@
-const GroupViewService = require(__dirname + '/../services/group-view.service');
-const {response} = require("../utils/response.utils");
+const GroupViewService = require('../services/group-view.service');
+const { response } = require('../utils/response.utils');
 const httpStatus = require('../enum/http-status');
 
-exports.get = async (req, res, next) => {
-    try {
-        const groupViews = await GroupViewService.get();
-        res.json(response(httpStatus.SUCCESS, groupViews));
-    } catch (e) {
-        next(e)
-    }
+exports.get = async (_req, res, next) => {
+  try {
+    const groupViews = await GroupViewService.get();
+    res.json(response(httpStatus.SUCCESS, groupViews));
+  } catch (e) {
+    next(e);
+  }
 };
 
 exports.getByGroupId = async (req, res, next) => {
-    try {
-        const groupViews = await GroupViewService.getByGroupId(req.query.groupId);
-        res.json(response(httpStatus.SUCCESS, groupViews));
-    } catch (e) {
-        next(e)
-    }
+  try {
+    const groupViews = await GroupViewService.getByGroupId(req.query);
+    res.json(response(httpStatus.SUCCESS, groupViews));
+  } catch (e) {
+    next(e);
+  }
 };
 
 exports.getAvailableLayers = async (req, res, next) => {
-    try {
-        const availableLayers = await GroupViewService.getAvailableLayers(req.query.groupId);
-        res.json(response(httpStatus.SUCCESS, availableLayers));
-    } catch (e) {
-        next(e)
-    }
+  try {
+    const availableLayers = await GroupViewService.getAvailableLayers(
+      req.query.groupId,
+    );
+    res.json(response(httpStatus.SUCCESS, availableLayers));
+  } catch (e) {
+    next(e);
+  }
 };
+
 exports.add = async (req, res, next) => {
-    try {
-        const newGroup = req.body;
-        const groupView = await GroupViewService.add(newGroup);
-        res.json(response(httpStatus.SUCCESS, groupView));
-    } catch (e) {
-        next(e)
-    }
-}
+  try {
+    const newGroup = req.body;
+    const groupView = await GroupViewService.add(newGroup);
+    res.json(response(httpStatus.SUCCESS, groupView));
+  } catch (e) {
+    next(e);
+  }
+};
 
 exports.update = async (req, res, next) => {
-    try {
-        const groupModify = req.body.params;
-        const result = await GroupViewService.update(groupModify)
-        res.json(response(httpStatus.SUCCESS, result));
-    } catch (e) {
-        next(e)
-    }
-}
+  try {
+    const groupModify = req.body.params;
+    const result = await GroupViewService.update(groupModify);
+    res.json(response(httpStatus.SUCCESS, result));
+  } catch (e) {
+    next(e);
+  }
+};
 
 exports.updateAdvanced = async (req, res, next) => {
-    try {
-        const groupModify = req.body.params;
-        const groupViews = await GroupViewService.updateAdvanced(groupModify);
-        res.json(response(httpStatus.SUCCESS, groupViews));
-    } catch (e) {
-        next(e)
-    }
-}
+  try {
+    const groupModify = req.body.params;
+    const groupViews = await GroupViewService.updateAdvanced(groupModify);
+    res.json(response(httpStatus.SUCCESS, groupViews));
+  } catch (e) {
+    next(e);
+  }
+};
--- routes/info-columns.router.js
+++ routes/info-columns.router.js
@@ -5,7 +5,7 @@ const InfoColumnsController = require('../controllers/info-columns.controller');
 router.get('/getAllTables', InfoColumnsController.getAllTables);
 router.get('/getSecondaryTypes', InfoColumnsController.getSecondaryTypes);
 router.get('/tableColumns?:tableId', InfoColumnsController.getTableColumns);
-router.get('/getInfocolumnsByTableName?:tableName', InfoColumnsController.getInfocolumnsByTableName);
+router.get('/getByTableName?:tableName', InfoColumnsController.getInfocolumnsByTableName);
 router.get('/getInfocolumnsByViewId?:tableName', InfoColumnsController.getInfocolumnsByViewId);
 router.get('/:viewId?', InfoColumnsController.getInfoColumns);
 router.put('/tableColumns?:editions*', InfoColumnsController.updateTableInfoColums);
