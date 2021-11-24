58345bd79a295fa8606ff8a43c1145074537b518
Kyle Felipe <kylefelipe@gmail.com>
2021-11-24 09:51:36 -0300

    Remove function to get exterior ring

--- services/biome.service.js
+++ services/biome.service.js
@@ -1,12 +1,9 @@
-const {Biome, sequelize} = require('../models');
+const { Biome, sequelize } = require("../models");
 
 exports.get = async () => {
   const options = {
-    attributes: [
-      'gid', 'name',
-      [sequelize.fn("ST_AsText", sequelize.fn("ST_ExteriorRing", sequelize.col('geom'))), 'wkt'],
-    ],
-    order: [['name']],
+    attributes: ["gid", "name"],
+    order: [["name"]],
   };
   return await Biome.findAll(options);
 };
